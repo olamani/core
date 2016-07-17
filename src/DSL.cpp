a1bb388114a373931731eaee2d5d46c04ebbab60
Nelson Iván González <nigm2005@gmail.com>
2016-07-17 01:33:23 -0500

    Adding regex builder

--- src/DSL.cpp
+++ src/DSL.cpp
@@ -1,10 +1,60 @@
 #include "DSL.hpp"
 #include <regex>
 #include <string>
+#include <sstream>
+#include <cctype>
 
 namespace Olamani {
 
 namespace DSL {
+
+bool islparenthesis(char c) {
+    return c == '(';
+}
+
+bool isrparenthesis(char c) {
+    return c == ')';
+}
+
+std::string double_regex = "([\\d\\.\\-e]+)";
+std::string int_regex = "(\\d+)";
+std::string string_regex = "(\\w+)";
+
+std::string sense_body = "(sense_body $i (parameter $d)$_(parameter $d))";
+
+std::string prepareRegex(std::string prepare) {
+    std::stringstream builder;
+    auto it = prepare.begin();
+    while (it != prepare.end()) {
+        char c = *it;
+        if (islparenthesis(c)) {
+            builder << "\\(";
+        } else if (isrparenthesis(c)) {
+            builder << "\\)";
+        } else if (c == ' ') {
+            builder << "\\s+"
+        } else if (c == '$') {
+            it++;
+            c = *it;
+            switch (c) {
+            case 'i':
+                builder << int_regex;
+            case 'd':
+                builder << double_regex;
+            case 's':
+                builder << string_regex;
+            case '_':
+                builder << "\\s*";
+            }
+        } else if (isalpha(c) || c == '_') {
+            builder << c;
+        }
+        it++;
+    }
+    return builder.str();
+}
+
+boost::regex sense_body_regex(prepareRegex(sense_body));
 
 int getIntParameter(std::string message, std::string parameter, int _default) {
     std::smatch match;
